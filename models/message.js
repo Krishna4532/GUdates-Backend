dcac77628f55edd6fd563c1326366b57e0113b05
Krishna4532 <ks6895216@gmail.com>
2025-11-16 04:32:03 +0530

    Update message.js

--- models/message.js
+++ models/message.js
@@ -1,16 +1,16 @@
-// backend/models/Message.js
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  text: { type: String, default: "" },
-  image: { type: String, default: "" }, // optional image url/base64
-  read: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
-});
+const messageSchema = new mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    message: String
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Message", messageSchema);
+
 
 
 
