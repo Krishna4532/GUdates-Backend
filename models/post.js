04185eedda938bae1fb338fcb9772b353731ceab
Krishna4532 <ks6895216@gmail.com>
2025-11-16 03:51:36 +0530

    Refactor post schema in models/post.js
    Refactored post schema to include user and image fields, and updated comments structure.

--- models/post.js
+++ models/post.js
@@ -1,21 +1,13 @@
 import mongoose from "mongoose";
 
-const commentSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  text: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
 const postSchema = new mongoose.Schema({
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  image: String,
   caption: String,
-  media: String,
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  comments: [commentSchema],
-  shares: { type: Number, default: 0 },
-  isCollab: { type: Boolean, default: false },
-  collabWith: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, text: String, createdAt: Date }],
   createdAt: { type: Date, default: Date.now }
 });
 
 export default mongoose.model("Post", postSchema);
+
