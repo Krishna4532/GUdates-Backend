89da33a446ef738076a94d3bb46008ea4a49600e
Krishna4532 <ks6895216@gmail.com>
2025-11-16 03:52:07 +0530

    Refactor story schema to use 'user' and 'image'

--- models/story.js
+++ models/story.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 const storySchema = new mongoose.Schema({
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  media: String,
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  image: String,
   expiresAt: Date,
   createdAt: { type: Date, default: Date.now }
 });
 
 export default mongoose.model("Story", storySchema);
+
+
